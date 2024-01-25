b13306ccab0f868e4e20ff545d8d487a43afce40
NewHopeJus <newhopejus@gmail.com>
2024-01-25 22:27:20 +0200

    Klausimo activity pridėtas toolbaras su home ir back buttonais

--- app/src/main/java/com/mastercoding/bakalaurinis/view/questions/QuestionActivity.java
+++ app/src/main/java/com/mastercoding/bakalaurinis/view/questions/QuestionActivity.java
@@ -3,9 +3,13 @@ package com.mastercoding.bakalaurinis.view.questions;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.mastercoding.bakalaurinis.R;
+import com.mastercoding.bakalaurinis.view.menus.MainMenuActivity;
 import com.mastercoding.bakalaurinis.view.menus.MainMenuListFragment;
 
 public class QuestionActivity extends AppCompatActivity {
@@ -21,14 +25,38 @@ public class QuestionActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             OneSelectionQuestionFragment oneSelectionQuestionFragment = new OneSelectionQuestionFragment();
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.question_fragment_container, oneSelectionQuestionFragment)
+                    .add(R.id.fragment_container_question_fragment, oneSelectionQuestionFragment)
                     .commit();
         }
 
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        //kad paslepti teksta toolbaro
-//        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        Toolbar toolbar = findViewById(R.id.toolbarQuestionActivity);
+        setSupportActionBar(toolbar);
+
+        //kad paslepti teksta toolbaro
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //toolbaro menu pridedam
+        getMenuInflater().inflate(R.menu.bottom_toolbar_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // atgal mygtukas ant toolbaro
+        //Griztam i lygio temu langa
+        if (item.getItemId() == R.id.action_back) {
+            finish();
+            return true;
+        } else if (item.getItemId() == R.id.action_home) {
+           //Griztam i pagrindini puslapi
+            Intent intent = new Intent(this, MainMenuActivity.class);
+            intent.putExtra("home", 0);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
